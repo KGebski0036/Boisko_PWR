bcff47280de2744309feed28f0d616d46b00720f
KGebski0036 <kgebski68@gmail.com>
2025-05-22 14:54:19 +0200

    css

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { ActiviteListComponent } from "./components/activite-list/activite-list.component";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [ActiviteListComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { FieldsComponent } from './pages/fields/fields.component';
+import { GamesComponent } from './pages/games/games.component';
+import { TeamsComponent } from './pages/teams/teams.component';
+import { AdminComponent } from './pages/admin/admin.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'fields', component: FieldsComponent },
+  { path: 'games', component: GamesComponent },
+  { path: 'teams', component: TeamsComponent },
+  { path: 'admin', component: AdminComponent },
+];
