8080959cb7034765723bb120276f8b7c3e36da1f
Paul0081 <pgryszczyk@gmail.com>
2025-06-05 15:35:59 +0200

    Zrobiona główna strona według projektu: teskt powitalny, zabezpieczenie i działające przyciski do podstron

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule, RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterOutlet, RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
